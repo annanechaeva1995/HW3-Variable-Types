aeb893c1369aa6aac33894fd91d9fd970e08753b
Anna Nechaeva <annanechaeva9595@gmail.com>
2023-01-09 13:26:12 +1000

    Задачи 1-2

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+System.out.println("Задачи 1-2");
+    int apples = 45000;
+    System.out.println("Значение переменной apples с типом int равно " + apples +".");
+    byte pears = 102;
+    System.out.println("Значение переменной pears с типом byte равно " + pears + ".");
+    short peaches = 2000;
+    System.out.println("Значение переменной peaches с типом short равно " + peaches + ".");
+    long nuts = 89500L;
+    System.out.println("Значение переменной nuts с типом long равно " + nuts + ".");
+    float sugar = 2.5f;
+    System.out.println("Значение переменной sugar с типом float равно " + sugar + ".");
+    double salt = 25.125;
+    System.out.println("Значение переменной salt с типом double равно " + salt + ".");
+
+    int A = 27897;
+    System.out.println(A);
+    byte B = 67;
+    System.out.println(B);
+    short C = 569;
+    System.out.println(C);
+    long D = 987678965549L;
+    System.out.println(D);
+    float E = 27.12F;
+    System.out.println(E);
+    double F = 2.786;
+    System.out.println(F);
+    short G = -159;
+    System.out.println(G);
+    }
+}
